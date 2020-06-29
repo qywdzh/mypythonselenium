9ee728d676b60b9c8cd4642e45b68d45f4070c2d
qywdzh <503811250@qq.com>
2020-06-29 13:19:04 +0800

    封装keyword

--- keyword_selenium/actionMethod.py
+++ keyword_selenium/actionMethod.py
@@ -1,6 +1,16 @@
 from selenium import  webdriver
 from base.find_element import FindElement
+import time
+
 class ActionMethod():
+
+    def __init__(self, browser, note, key, fileName=None):
+        self.driver = self.open_browser(browser)
+        self.element = FindElement(self.driver)
+        self.note = note
+        self.key = key
+        self.fileName = fileName
+
     def open_browser(self, browser):
         """
         打开浏览器
@@ -15,12 +25,41 @@ class ActionMethod():
             driver = webdriver.Chrome()
         return driver
 
-    def get_url(self, browser, url):
+    def get_url(self, url):
         """
 
         :param browser:
         :param url:
         :return:
         """
-        self.open_browser(browser).get_url(url)
+        self.driver.get(url)
+
+    def element_sleep(self):
+        """
+        设置等待时间
+        :return:
+        """
+        time.sleep(2)
+
+    def get_element(self):
+        """
+        获取元素
+        :return:
+        """
+        get_element = self.element.find_element(self.note, self.key, filename=self.fileName)
+        return get_element
+
+    def send_values(self, values):
+        """
+        输入action
+        :return:
+        """
+        self.get_element().send_keys(values)
+
+    def click_element(self):
+        """
+        点击action
+        :return:
+        """
+        self.get_element().click()
 
